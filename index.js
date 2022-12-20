41f8f62ca79c3f1998d0a147dc1ea19874c9593c
Jose David <josedvd123@gmail.com>
2022-12-20 08:11:58 +0100

    Fix: Simplify code example

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const ethers = require('ethers')
-const moment = require("moment");
-
+const moment = require("moment")
 
 const json = [
     {
@@ -19,8 +18,8 @@ const json = [
         }
     }
 ]
-const hash = hashJson(json)
 
+const hash = hashJson(json)
 
 // sendHashToBlockchain(hash);
 async function sendHashToBlockchain(hash) {
@@ -29,86 +28,67 @@ async function sendHashToBlockchain(hash) {
     try {
         const tx = await contract.setHashInfo(hash)
 
-        console.log('PENDING TRANSACTION',
-            {
-                hash,
-                tx: `https://sepolia.etherscan.io/tx/${tx.hash}`
-            })
+        console.log('PENDING TRANSACTION', {
+            hash,
+            tx: `https://sepolia.etherscan.io/tx/${tx.hash}`
+        })
 
         await tx.wait()
 
-        console.log('TRANSACTION SUCCESS',
-            {
-                hash,
-                tx: `https://sepolia.etherscan.io/tx/${tx.hash}`
-            })
+        console.log('TRANSACTION SUCCESS')
     } catch (e) {
         console.log(e.message)
     }
 }
 
-listenHashEvents()
 
-function listenHashEvents() {
-    const contract = connectToSmartContract()
-
-    contract.on('LogNewHash', (hash, ownerAddress, creationDt) => {
-        console.log('BLOCKCHAIN EVENT',
-            {
-                hash,
-                ownerAddress,
-                creationDt: moment(creationDt.toString(), 'X').format('DD/MM/YYYY HH:mm:ss')
-            })
-    })
-}
-
-
-// getHashInfoFromBlockchain('0xd460e455675b674711e0078eb6aa2f9106a12ce7c4c8ad39ff38c359d995cfc1')
+// getHashInfoFromBlockchain(hash)
 async function getHashInfoFromBlockchain(hash) {
     const contract = connectToSmartContract()
-
     const [ownerAddress, timestampBN] = await contract.getHashInfo(hash)
-
     const hashInfo = {
         hash,
         ownerAddress,
-        creationDt: moment(timestampBN.toString(), 'X').format('DD/MM/YYYY HH:mm:ss')
+        creationDt: moment(timestampBN.toString(), 'X').format('DD/MM/YYYY HH:mm:ss') // Timestamp a datetime
     }
 
     console.log('GET HASH INFO', {hashInfo})
-
-    return hashInfo;
 }
 
 
 function connectToSmartContract() {
-    const abi = require('./contract-abi.json') // Es un JSON con todas las funciones del contrato inteligente
-
     const provider = new ethers.providers.JsonRpcProvider(
         'https://rpc.sepolia.org' // Servidor que nos permite comunicarnos con la blockchain remotamente
     )
-
     const wallet = new ethers.Wallet(
         '216a598f03e036016c5dfeaf397eb4f0a6c490737820e3ce88df717e424bd148',
         provider
     )
-
     const contract = new ethers.Contract(
         '0xF055A06FAe23e6613d59bd01379Ae3c13eFF3ca9',
-        abi,
+        require('./contract-abi.json'), // Es un JSON con todas las funciones del contrato inteligente
         wallet)
 
     return contract;
 }
 
 
-/***
- * Creamos un hash KECCAK256 desde un JSON, una alternativa seria un hash SHA256
- */
+function listenHashEvents() {
+    const contract = connectToSmartContract()
+
+    contract.on('LogNewHash', (hash, ownerAddress, creationDt) => {
+        console.log('BLOCKCHAIN EVENT', {
+            hash,
+            ownerAddress,
+            creationDt: moment(creationDt.toString(), 'X').format('DD/MM/YYYY HH:mm:ss')
+        })
+    })
+}
+
+
+// Creamos un hash KECCAK256 desde un JSON, una alternativa seria un hash SHA256
 function hashJson(json) {
-    return ethers.utils.id(
-        JSON.stringify(json)
-    )
+    return ethers.utils.id(JSON.stringify(json))
 }
 
 
